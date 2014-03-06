3bd27d7c1455caca690dc651787b104d7429399d
Daniel McGlinn <danmcglinn@gmail.com>
2014-03-06 10:20:30 -0700

    Add script to compare the GBIF list of query names and Tank tree tip names.

---
+++ scripts/check_query_names_against_tree.R
@@ -0,0 +1,20 @@
+## purpose: to compare the list of names sent to GBIF in 2011 with 
+## those names on the Tank phylo tree
+
+library(ape)
+
+sp = read.table('./query_names/Zanne_GBIF_taxa_list_Aug2011.txt', sep=',')
+sp = as.character(sp[ , 1])
+sp = sub(' ', '_', sp)
+
+tree = read.tree('../bigphylo//Tank-tree/Vascular_Plants_rooted.dated.tre')
+
+sum(tree$tip.label %in% sp)
+#[1] 22926
+length(tree$tip.label)
+#[1] 31749
+
+## so all the names in the query list are not on the tree
+## this is possibly because of synonymy issues?
+
+
