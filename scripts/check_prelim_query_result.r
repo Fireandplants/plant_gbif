46d06fb31ca1290723124628d6ca8b825932940b
Dylan W. Schwilk <dylan@schwilk.org>
2014-03-18 17:59:32 -0500

    Added version of prelim query result without "Key error" lines and short script to count number of names with 100 or more records or 50 or more.

---
+++ scripts/check_prelim_query_result.r
@@ -0,0 +1,11 @@
+# 2014-03-18 
+# schwilk
+
+prelim.res <- read.csv("../query_names/GBIF_lookup-cleaned.txt", sep="\t", header=FALSE)
+names(prelim.res) <- c("GBIF.taxon.key", "kingdom", "family", "scientific.name", "qname", "nrecords" )
+
+more50 <- subset(prelim.res, nrecords >= 50)
+length(more50$scientific.name)
+
+more100 <- subset(prelim.res, nrecords >= 100)
+length(more100$scientific.name)
