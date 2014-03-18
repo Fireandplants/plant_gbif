0248fcf2d7ff58d8492ee9a800329e0a9d61a5a7
Dylan W. Schwilk <dylan@schwilk.org>
2014-03-18 18:00:11 -0500

    add result as comment

--- scripts/check_prelim_query_result.r
+++ scripts/check_prelim_query_result.r
@@ -6,6 +6,9 @@ names(prelim.res) <- c("GBIF.taxon.key", "kingdom", "family", "scientific.name",
 
 more50 <- subset(prelim.res, nrecords >= 50)
 length(more50$scientific.name)
+#[1] 55245
+
 
 more100 <- subset(prelim.res, nrecords >= 100)
 length(more100$scientific.name)
+#[1] 44079
