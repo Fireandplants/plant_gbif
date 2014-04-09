c28be985d14a78d92b52022bd35121a92dedd3fe
Dylan W. Schwilk <dylan@schwilk.org>
2014-04-09 12:53:04 -0500

    Add example output to expand-tanknames.sh
    Added a bash one-line (commented out) that when run, shows that the
    synonymize.py -a expand and synonymize.py -a merge steps are inverse
    functions (although in theory, with certain illogical and bad combinations of
    synonyms as a "canonical list" this is not guaranteed as information could be
    lost!)

--- scripts/expand-tanknames.sh
+++ scripts/expand-tanknames.sh
@@ -3,7 +3,13 @@
 TANKNAMES="../../bigphylo/species/big-phylo-leaves.txt"
 EXPANDED_NAMES="../query_names/taxa_for_bigphylo_gbif_query_04_08_14.txt"
 
-python synonymize.py -a expand $TANKNAMES >> ../query_names/taxa_for_bigphylo_gbif_query_04_08_14.txt
+python synonymize.py -a expand $TANKNAMES >> $EXPANDED_NAMES
 
 # to merge the result, in $EXPANDED NAMES
-#python synonymize.py -a merge -c $TANKNAMES $EXPANDED_NAMES >> ../query_names/merged_names.txt
+# python synonymize.py -a merge -c $TANKNAMES $EXPANDED_NAMES >> ../query_names/merged_names.txt
+
+# proof this is reversible :
+# diff -s <(sort $TANKNAMES) <(python synonymize.py -a merge -c $TANKNAMES $EXPANDED_NAMES | sort | uniq)
+
+## output: Files /dev/fd/63 and /dev/fd/62 are identical
+
