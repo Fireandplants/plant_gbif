53248d91e8321ccad9669ccbf8a51df37a32ffe3
Dylan W. Schwilk <dylan@schwilk.org>
2014-04-09 14:01:05 -0500

    Check new expanded tanknames list older list
    Check against with March 2014 GBIF query. See #7.

--- scripts/check_query_names_against_tree.R
+++ scripts/check_query_names_against_tree.R
@@ -3,11 +3,11 @@
 
 library(ape)
 
-sp = read.table('./query_names/Zanne_GBIF_taxa_list_Aug2011.txt', sep=',')
+sp = read.table('../query_names/Zanne_GBIF_taxa_list_Aug2011.txt', sep=',')
 sp = as.character(sp[ , 1])
 sp = sub(' ', '_', sp)
 
-tree = read.tree('../bigphylo/Tank-tree/Vascular_Plants_rooted.dated.tre')
+tree = read.tree('../../bigphylo/Tank-tree/Vascular_Plants_rooted.dated.tre')
 
 sum(tree$tip.label %in% sp)
 #[1] 22926
@@ -19,12 +19,31 @@ length(tree$tip.label)
 
 missing_tips = tree$tip.label[!(tree$tip.label %in% sp)]
 
-write.table(missing_tips, file='./query_names/missing_tank_tree_tips.txt',
+write.table(missing_tips, file='../query_names/missing_tank_tree_tips.txt',
             row.names=F, col.names=F, quote=F)
 
 ## generate a complete list to send for a gbif query----------------------------
 all_sp = unique(c(tree$tip.label, sp))
 all_sp = sub('_', ' ', all_sp)
 
-write.table(all_sp, file='./query_names/taxa_for_big_phylo_gbif_query_03_12_2014.txt',
-            row.names=F, col.names=F, quote=F) 
+write.table(all_sp,
+            file='../query_names/taxa_for_big_phylo_gbif_query_03_12_2014.txt',
+            row.names=F, col.names=F, quote=F)
+
+
+## Check this list against the new one created using synonymize.py and TPL1.1
+## data
+
+tanknames.expanded <- read.table("../query_names/taxa_for_bigphylo_gbif_query_04_08_14.txt", sep = ",",)
+tanknames.expanded = as.character(tanknames.expanded[ , 1])
+
+not.in.new <- all_sp[!(all_sp %in% tanknames.expanded)]
+length(not.in.new)
+
+## what? THere are a lot of taxa in the Zanne list that are not in the tank
+## tree and don't ahve synonyms according to TPL1.1 Perhaps because The Zanne
+## et al list included potential gender changes? eg "Abarema adenophora" is in
+## the Zanne list, is not in the tank tree, and is not a synonym or "sister
+## synonym" of anything in the tank tree. It is an accepted name as is "Abarema
+## adenophorum" -- perhaps the Zanne list included this as a synonym based on
+## matching? I'm not sure
