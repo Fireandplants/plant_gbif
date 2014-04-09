7447f4707bf3ab193ecdea8ba01f509b9a3d2635
Dylan W. Schwilk <dylan@schwilk.org>
2014-04-08 22:25:41 -0500

    Expand and merge. Almost there.

--- scripts/synonymize.py
+++ scripts/synonymize.py
@@ -4,6 +4,9 @@
 lookup, expand a canonical list or merge list containing synonyms.
 
 NEEDS testing still.
+
+This looks good Beth.  I have explored this some.  One thing that I discovered: the original problem I noted when opening this issue still holds, but it is simply characteristic of TPL.  The statement "there is ONE accepted name for each unique name", is only true when one includes authors. In fact, ignoring authorship, there are some synonyms that match to multiple accepteds (eg "Amaryllis dubia").  No way around it, but it does mean that expanding a canonical list is not completely reversible, one cannot merge back to the same set of names exactly.  Well, I'm working on a solution one, but it is a more complicated problem than searching through lists of 'sister' synonyms.  It is not the end of the world in any case
+
 """
 
 __version__ =    '''0.1'''
@@ -11,52 +14,70 @@ __program__ =    '''synonymize.py'''
 __author__  =    '''Dylan Schwilk'''
 __usage__   =    '''synonymize.py [options] [names_file]'''
 
-# default files if non given
-TPL_FILE = "../theplantlist1.1/TPL1.1_synonymy_list"
-NAMES_FILE = "../../bigphylo/species/big-phylo-leaves.txt"
-
 import logging
 logging.basicConfig(format='%(levelname)s: %(message)s')
 tpl_logger = logging.getLogger('tpl_logger')
 
+# The PLant List synonymy table:
+TPL_FILE = "../theplantlist1.1/TPL1.1_synonymy_list"
+# default canonical names file if none given
+CANONICAL_NAMES_FILE = "../../bigphylo/species/big-phylo-leaves.txt"
+
+# global dicts
+syn2accepted = {}
+accepted2syn = {}
+
 def read_names(src):
-    return([line.rstrip() for line in src.readlines()])
+    return([line.rstrip() for line in src])
 
-def tpl_dict(tpl):
-    """Create dictionary from the tpl ragged array.
+def make_tpl_dicts(tpl):
+    """Create dictionary from the tpl ragged array
+
     """
-    syn = {}
-    for line in open(TPL_FILE):
+    
+    for line in tpl:
         names = line[:-1].replace("_"," ").split(",")  # replace underscores with spaces
-        a =names[0]
-        #syn[a] = a
-        for s in names[1:]:
-            syn[s] = a # should be one accepted per synonym
-    return(syn)
+        syns = set(names)
+        a = names[0]
+        accepted2syn[a] = syns
+        for n in syns :
+            syn2accepted[n] = a
 
-# def reverse_dict(d):
-#     r = {}
-#     for key,s in d.iteritems():
-#         for n in s.items():
-#             r[s] =
-
-def expand_names(canonical, d):
+            
+def expand_names(names):
     r = set()
-    for name in canonical:
-        r.add(name)
-        r.add(d.get(name, name))
+    for name in names:
+        r.add(name) # add itself
+        if syn2accepted.has_key(name): # is it even in TPL?
+            syns = accepted2syn[syn2accepted[name]] # if so, get all sister synonyms
+            for n in syns:
+                r.add(n)
     return(r)
 
 
-def merge_names(badnames, d):
-    """Merge list of names. Return a list of the same length as badnames, but with
-synonyms replaced. If synonym not found, use actual name in badnames."""
-    good = []
-    for n in badnames:
-        good.append(d.get(n,n))
-    return(good)
+def merge_names(badnames, goodnames):
+    """Merge list of names using list or set "goodnames" as canonical names.
+    Modifies badnames, but with synonyms replaced. If synonym not found, use
+    actual name in badnames.
 
-    
+    """
+    g = set(goodnames)
+    for i,n in enumerate(badnames):
+        if not n in g :  # name is not already canonical
+            # 2 possibilities: it is an accepted name or a synonym
+            a = syn2accepted[n]
+            if a in g : # accepted is canonical
+                badnames[i] = a
+            else : # last try accepted is a sister synonym
+                syns = accepted2syn[a] # get syns of a
+                snames = syns.intersection(g)
+                if len(snames) > 0 :  # might was well take first one, no way to choose:
+                    badnames[i] = snames.pop()
+                # if this doesn't work, stick with original
+                else :
+                    print("Not found: " + n)
+    return
+
 def main():
     '''Command line program.  '''
     import sys   
@@ -86,9 +107,8 @@ def main():
     elif len(args) == 1:
         names = read_names(sys.stdin)
 
-
     # make the lookup
-    syndict = tpl_dict(options.TPL_FILE)
+    syndict, accepteddict = tpl_dict(open(options.TPL_FILE))
 
     # expand or merge
     if options.action=="expand" :
