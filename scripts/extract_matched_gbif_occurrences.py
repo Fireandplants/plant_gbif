7368a6c34703f731c53aedef21622af67c287795
Dylan W. Schwilk <dylan@schwilk.org>
2014-09-07 00:03:50 -0500

    Extract all matching GBIF records
    New script to step through big GBIF Plantae data file and pull all
    records with a lat and lon that also match a name int he final fuzzy
    match lookup table. This did require some local changes to synonymize.py
    to allow looking up the canonical name at each line rather than using
    the "merge" action provided by the synonymize.py command-line script.

--- scripts/extract_matched_gbif_occurrences.py
+++ scripts/extract_matched_gbif_occurrences.py
@@ -1,31 +1,26 @@
-
-
 ## script to cycle through entire huge gbif plantae data dump and check each
+## name against the fuzzy match table (expanded tank et al names to gbif names
+## lookup).
 
-## name against tow alternative lookups, Jan Legin's synonym table via the gbif
-## backbone phylogeny and our own expanded takname to gbif lookup created by
-## fuzzy match
- 
-#import pandas as pd
+## Note that rather than using the "merge" action provided by the command line
+## synonymize.py script, this script imports synonymize and uses some functions
+## directly. This is in the interest of speed: it takes along time to go
+## through 132 million gbif records, let's only do it once and get the
+## canonical name while we are visiting each record.
+
 import zipfile as zf
 import synonymize
 import codecs
 
-legindFile =       codecs.open("../query_names/nescent_interpreted_names.csv", "r", "utf-8")
 fuzzyMatchesFile = codecs.open("../query_names/gbif_tank_lookup_final.csv","r", "utf-8")
 fieldsFile =       codecs.open("../query_names/gbif_fields.txt", "r", "utf-8")
 
+TANKNAMES= codecs.open("../../bigphylo/species/big-phylo-leaves.txt", "r", "utf-8")
+goodnames = synonymize.read_names(TANKNAMES)
+goodnames = set(goodnames)
 
 ## make tpl dicts
 synonymize.make_tpl_dicts(codecs.open(synonymize.TPL_FILE, "r", "utf-8"))
-
-## create lnames lookup table:
-lnames = {}
-legindFile.readline() # throw away header
-for l in legindFile:
-    fields = l.split(",")
-    lnames[ fields[1][1:-1] ] = fields[0][1:-1] # lookup accepted to searched
-
 
 ## create fuzzy matches lookup table
 fnames = {}
@@ -33,7 +28,6 @@ fuzzyMatchesFile.readline()
 for l in fuzzyMatchesFile:
     fields = l.split(",")
     fnames[ fields[0][1:-1] ] = fields[1][1:-1] # lookup accepted to searched
-
 
 gfields = fieldsFile.readlines()
 gfields = map(lambda x: x.strip(), gfields)
@@ -49,40 +43,33 @@ for i,h in enumerate(gbif_header) :
 #print(hdict)
 
 occurences = zf.ZipFile('/mnt/gis/gbif_plantae/0002274-140616093749225.zip').open("occurrence.txt", "r")
-output_file = codecs.open('gbif-occurrences_extracted_140905.csv', 'w', "utf-8")
-output_file.write("match_source\tsearchname\tgbifname\ttankname\t")
+output_file = codecs.open('gbif-occurrences_extracted_140906.csv', 'w', "utf-8")
+output_file.write("gbifname\texpandedname\ttankname\t")
 for h in gfields:
             output_file.write(h + "\t")
 output_file.write("\n")
  
 n = 0
-nfound = 0
-nnotfound = 0
+nmatches=0
 for l in occurences:
     l = codecs.decode(l, "utf-8")
     n = n + 1 
-    if n == 1 : 
-        continue
-        
+    
     f = l[:-1].split("\t")
-    found = False
-    # check against Jan Legind's list
+    # first check if lat and lon exist 
+    if not f[hdict["decimalLatitude"]] or not f[hdict["decimalLongitude"]]:
+        continue
+
     name = f[hdict["species"]] 
-    res =  lnames.get(name)
+    res =  fnames.get(name)
     if res :
-        found = True
-    #    print("found le: " + name + ", " + res)
-        resline = "legind\t" + name + "\t" + res + "\t"
-    else :
-        res =  fnames.get(name)
-        if res :
-    #        print("found fm: " + name + ", " + res)
-            found = True
-            resline = "fmatch\t" + name + "\t" + res + "\t"
-            
-    if found :  # get all the needed fields
-        if (n % 1000 == 0) : print(str(n) + ": " + resline)
-        tankname = synonymize.bad2good(res)
+        nmatches = nmatches+1
+        # print("found fm: " + name + ", " + res)
+        resline = name + "\t" + res + "\t"
+        
+        # get all the needed fields
+        if (nmatches % 1000 == 0) : print(str(n) + "\t" + str(nmatches) +": " + resline)
+        tankname = synonymize.bad2good(res, goodnames)
         resline = resline + tankname + "\t"
         for h in gfields:
        #     print(h)
@@ -90,6 +77,6 @@ for l in occurences:
             resline = resline + f[hdict[h]] + "\t"
         output_file.write(resline + "\n")
 
-#    if n > 20000 : break
+#    if n > 10000 : break
 
 
