29324facb90f8b6e56952d70a540291320089774
Dan McGlinn <danmcglinn@gmail.com>
2015-03-14 15:55:52 -0400

    Update code block headers on run all script and change to using larger chunks of the data to decrease the number of genera specific files that are generated downstream.

--- scripts/geog_filter/run_all.R
+++ scripts/geog_filter/run_all.R
@@ -13,39 +13,39 @@
 dir.create('./log_files')
 dir.create('./data/gbif_chunks')
 
-## Part I - Break up GBIF data dump into smaller files
+## Part I - Break up GBIF data dump into smaller files-----------------------
 script_file = './scripts/split-csv.py'
 options = '-v -d'
-nlines = '-n 500000'
+nlines = '-n 1000000'
 log_file = './log_files/chunk_gbif.log'
-input_file = './data/gbif-occurrences_extracted_141030.csv'
+input_file = './data/gbif-occurrences_extracted_150311.csv'
 
 cmd = paste('python', script_file, options, nlines,
             '-o./data/gbif_chunks/chunk-', input_file, '>', log_file, '2>&1')
 system(cmd, wait=F)
 
-## Part II - Prepare remotely sensed data for queries
+## Part II - Prepare remotely sensed data for queries------------------------
 script_file = './scripts/geog_filter/setup_geog_data.R'
 log_file = './log_files/setup_geog_data.log'
 
 cmd = paste('Rscript', script_file, '>', log_file, '2>&1')
 system(cmd, wait=F)
 
-## Part III - Geographically filter dataset
+## Part III - Geographically filter dataset----------------------------------
 script_file = './scripts/geog_filter/geog_filter.R'
 log_file = './log_files/geog_filter.log'
 
 cmd = paste('Rscript', script_file, '>', log_file, '2>&1')
 system(cmd, wait=F)
 
-## Part IV - Query remotely sensed data
+## Part IV - Query remotely sensed data--------------------------------------
 script_file = './scripts/geog_filter/climate_query.R'
 log_file = './log_files/climate_query.log'
 
 cmd = paste('Rscript', script_file, '>', log_file, '2>&1')
 system(cmd, wait=F)
 
-## Part V - compile all records, drop remaining duplicates, and export
+## Part V - Compile all records, drop remaining duplicates, and export-------
 script_file = './scripts/geog_filter/export_all_records.R'
 log_file = './log_files/export_all_records.log'
 
