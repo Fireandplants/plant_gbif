4d18dc053f894b8284641b2b7d0606f2847134f3
Dylan W. Schwilk <dylan@schwilk.org>
2016-04-07 08:40:47 -0500

    Allow fuzzy match script to use taxon-name-utils

--- scripts/make_myco_gbif_fuzzy_lookup.py
+++ scripts/make_myco_gbif_fuzzy_lookup.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
+# Dylan W. Schwilk
 
 """Use fuzzy_match.py to match taxon names in gbif occurrence data set to
-expanded tanknames list.
+expanded name list.
 
 """
-# Dylan W. Schwilk 
+
 import codecs, datetime
+# from taxon-names-utils:
+import sys
+sys.path.insert(0, '../../taxon-name-utils/scripts') # to provide scripts in taxon-name-utils
 from synonymize import read_names
 from fuzzy_match import fuzzy_match_name_list
 
@@ -19,7 +23,7 @@ gbifnames = read_names(codecs.open("../query_names/gbif-occurrences-names_151014
 gbif_lookup_file = "../query_names/gbif_myco_lookup_151015.csv"
 
 outf = codecs.open(gbif_lookup_file, "w", "utf-8")
-#print(gbifnames[1:10])
+# print(gbifnames[1:10])
 print("START " + str(datetime.datetime.now()))
 res = fuzzy_match_name_list(gbifnames, tanknames, outf)
 print("DONE " + str(datetime.datetime.now()))
